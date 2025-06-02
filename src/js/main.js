b5a23f401af11acc5614f6884f5e1e404e5e80cd
amitkmr-123 <amit.kumar@thewebplant.in>
2025-06-02 15:17:08 +0530

    Footer Updates -- Done

--- src/js/main.js
+++ src/js/main.js
@@ -31,3 +31,41 @@ window.addEventListener('scroll', function () {
     document.body.classList.remove('page-scrolled');
   }
 });
+
+// 
+
+var images = document.querySelectorAll('.ftr .page-center .custom_logo img.hs-image-widget, .hdr .logo img.hs-image-widget');
+Array.prototype.slice.call(images).forEach(function (img, index) {
+  if (images[index].hasAttribute('src')) {
+    images[index].setAttribute('src', images[index].getAttribute('src').split('?')[0]);
+  }
+  var logoSrcSet = img.getAttribute('srcset');
+  if (logoSrcSet) {
+    var srcsetArr = logoSrcSet.split(', ');
+    var updatedSrcSetArr = [];
+    srcsetArr.forEach(function (src) {
+      updatedSrcSetArr.push(src.split('?')[0]);
+    });
+    img.setAttribute('srcset', updatedSrcSetArr.join(', '));
+  }
+});
+
+function headerHeight() {
+  const innerHdr = document.querySelector('.hdr .inner_hdr');
+  const hdr = document.querySelector('.hdr');
+
+  if (innerHdr && hdr) {
+    const style = getComputedStyle(innerHdr);
+    const marginTop = parseFloat(style.marginTop) || 0;
+    const marginBottom = parseFloat(style.marginBottom) || 0;
+    const totalHeight = innerHdr.offsetHeight + marginTop + marginBottom;
+
+    hdr.style.minHeight = totalHeight + 'px';
+  }
+}
+
+window.addEventListener('load', headerHeight);
+window.addEventListener('resize', () => {
+  clearTimeout(window._resizeTimer);
+  window._resizeTimer = setTimeout(headerHeight, 100);
+});
